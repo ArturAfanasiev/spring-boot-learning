e49e377003e1c9ffa586f0507221251babf9ae99
arturafanasiev <afanasievartur245@gmail.com>
2025-03-16 18:44:07 +0100

    created customer confirmation

--- src/main/java/com/luv2code/spingdemo/mvc/CustomerController.java
+++ src/main/java/com/luv2code/spingdemo/mvc/CustomerController.java
@@ -1,14 +1,31 @@
 package com.luv2code.spingdemo.mvc;
 
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CustomerController {
+
     @GetMapping("/")
     public String showForm(Model theModel){
         theModel.addAttribute("customer", new Customer());
         return "customer-form";
+    }
+    @PostMapping("/processForm")
+    public String ProcessForm(
+            @Valid @ModelAttribute("customer") Customer customer,
+            BindingResult theBindingResult){
+        if(theBindingResult.hasErrors()){
+            return "customer-form";
+        }
+        else{
+            return "customer-confirmation";
+        }
+
     }
 }
