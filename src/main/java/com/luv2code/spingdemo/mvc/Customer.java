9bf48cb7b8136e07dff14d7cd1df39039818e7ad
arturafanasiev <afanasievartur245@gmail.com>
2025-03-17 11:01:52 +0100

    maded freepasses required

--- src/main/java/com/luv2code/spingdemo/mvc/Customer.java
+++ src/main/java/com/luv2code/spingdemo/mvc/Customer.java
@@ -10,21 +10,27 @@ public class Customer {
     @Size(min=1, message="is required")
     private String lastName = "";
 
+    @NotNull(message="is required")
     @Min(value=0, message="must be greater than or equal to zero")
     @Max(value=10, message="must be less than or equal to 10")
-    private int freePasses;
+    private Integer freePasses;
 
-    @Pattern(regexp = "^[a-zA-z0-9]{5}", message = "only 5 chars/digits")
+    @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 chars/digits")
     private String postalCode;
 
+    public String getPostalCode() {
+        return postalCode;
+    }
 
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
 
-
-    public int getFreePasses() {
+    public Integer getFreePasses() {
         return freePasses;
     }
 
-    public void setFreePasses(int freePasses) {
+    public void setFreePasses(Integer freePasses) {
         this.freePasses = freePasses;
     }
 
@@ -36,7 +42,6 @@ public class Customer {
         this.firstName = firstName;
     }
 
-
     public String getLastName() {
         return lastName;
     }
@@ -44,11 +49,4 @@ public class Customer {
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
-    public void setPostalCode(String postalCode) {
-        this.postalCode = postalCode;
-    }
-    public String getPostalCode() {
-        return postalCode;
-    }
-
 }
