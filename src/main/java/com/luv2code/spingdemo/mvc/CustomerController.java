505518fcff192db57f8d437dbed2026e74b4abac
arturafanasiev <afanasievartur245@gmail.com>
2025-03-17 19:56:53 +0100

    created custom validation rule

--- src/main/java/com/luv2code/spingdemo/mvc/CustomerController.java
+++ src/main/java/com/luv2code/spingdemo/mvc/CustomerController.java
@@ -33,6 +33,8 @@ public class CustomerController {
             @Valid @ModelAttribute("customer") Customer customer,
             BindingResult theBindingResult){
         System.out.println("last name: |"+ customer.getLastName());
+        System.out.println("Binding result:" + theBindingResult.toString());
+        System.out.println();
         if(theBindingResult.hasErrors()){
             return "customer-form";
         }
